708a6a752a0bb5039cfd1bb28644b52254489f02
Beknazar007 <saitovbeknazar6@gmail.com>
2023-03-03 17:58:26 +0600

    adding ecr policy

--- AWS/modules/eks/roles.tf
+++ AWS/modules/eks/roles.tf
@@ -21,6 +21,7 @@ resource "aws_iam_role" "eks_cluster" {
 POLICY
 }
 
+
 resource "aws_iam_role_policy_attachment" "amazon_eks_cluster_policy" {
   # This policy will give permissions to eks itself
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
@@ -28,6 +29,7 @@ resource "aws_iam_role_policy_attachment" "amazon_eks_cluster_policy" {
   # The role the policy should be applied to
   role = aws_iam_role.eks_cluster.name
 }
+
 
 ##############################################################################################################
 
@@ -73,6 +75,34 @@ resource "aws_iam_role_policy_attachment" "amazon_eks_cni_policy_general" {
 resource "aws_iam_role_policy_attachment" "amazon_ec2_container_registry_read_only" {
   # This policy will allow us to download the private images from ECR repository.
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+
+  # The role the policy should be applied to
+  role = aws_iam_role.nodes_general.name
+}
+resource "aws_iam_policy" "ecr-policy" {
+  name        = "ecr-policy"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:BatchGetImage",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:GetAuthorizationToken"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_ec2_container_registry_read_only" {
+  # This policy will allow us to download the private images from ECR repository.
+  policy_arn = aws_iam_policy.ecr-policy.arn
 
   # The role the policy should be applied to
   role = aws_iam_role.nodes_general.name
