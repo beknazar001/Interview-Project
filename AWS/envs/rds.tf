4e7ac1bee5754c997ada56a433518276bd7b8a5d
Nurbolot <you@example.com>
2023-07-11 19:48:18 +0600

    first commit

--- AWS/envs/rds.tf
+++ AWS/envs/rds.tf
@@ -1,23 +1,85 @@
 #########################################################
 ###############       RDS Module           ##############
 #########################################################
+# module "db" {
+#   source                = "../../modules/aws_rds"
+#   identifier            = "demo"
+#   engine                = "postgres"
+#   engine_version        = "14"
+#   instance_class        = "db.t3.micro"
+#   allocated_storage     = "20"
+#   max_allocated_storage = "100"
+#   username              = "postgres"
+#   password              = "postgres"
+#   port                  = "5432"
+#   multi_az              = false
+#   publicly_accessible   = false
+#   deletion_protection   = false
+
+#   tags = {
+#     "env" = "${var.env}"
+#   }
+
+#   db_option_group_name = "postresql-option-group"
+#   description          = "postgresql-option-group"
+#   major_engine_version = "14"
+#   # options              = []
+
+#   subnet_group_name               = "postresql-subnet-group"
+#   subnet_group_description        = "postresql-subnet-group"
+#   vpc_id                          = module.networking.vpc_id
+#   subnet_ids                      = [module.networking.private_subnets[2], module.networking.private_subnets[3]]
+#   enabled_cloudwatch_logs_exports = ["upgrade"]
+#   rds_inbound = [
+#     {
+#       port        = 5432
+#       protocol    = "tcp"
+#       cidr_blocks = ["${module.networking.bastion_internal_ip}/32"]
+#     },
+#     {
+#       port        = 22
+#       protocol    = "tcp"
+#       cidr_blocks = ["${module.networking.bastion_internal_ip}/32"]
+
+#     }
+#     ,
+#     {
+#       port        = 0
+#       protocol    = "all"
+#       cidr_blocks = ["192.168.0.0/16"]
+
+#     }
+#   ]
+
+#   rds_outbound = var.outbound_all
+#   security_tags = {
+#     Name = var.security_group_tag
+#   }
+# }
+
+# provider "kubernetes" {
+#   host                   = module.eks.endpoint
+#   cluster_ca_certificate = module.eks.cacert
+#   token                  = module.eks.token
+# }
+
 module "db" {
   source                = "../../modules/aws_rds"
-  identifier            = "demo"
-  engine                = "postgres"
-  engine_version        = "14"
-  instance_class        = "db.t3.micro"
-  allocated_storage     = "20"
-  max_allocated_storage = "100"
-  username              = "postgres"
-  password              = "postgres"
-  port                  = "5432"
-  multi_az              = false
-  publicly_accessible   = false
-  deletion_protection   = false
+  identifier            = var.identifier
+  engine                = var.engine
+  engine_version        = var.engine_version
+  instance_class        = var.instance_class
+  allocated_storage     = var.allocated_storage
+  max_allocated_storage = var.max_allocated_storage
+  username              = var.username
+  password              = var.password
+  port                  = var.port
+  multi_az              = var.multi_az
+  publicly_accessible   = var.publicly_accessible
+  deletion_protection   = var.deletion_protection
 
   tags = {
-    "env" = "${var.env}"
+    "env" = var.env
   }
 
   db_option_group_name = "postresql-option-group"
@@ -27,8 +89,8 @@ module "db" {
 
   subnet_group_name               = "postresql-subnet-group"
   subnet_group_description        = "postresql-subnet-group"
-  vpc_id                          = module.networking.vpc_id
-  subnet_ids                      = [module.networking.private_subnets[2], module.networking.private_subnets[3]]
+  vpc_id                          = data.aws_vpc.default.id
+  subnet_ids                      = data.aws_subnet_ids.default.ids
   enabled_cloudwatch_logs_exports = ["upgrade"]
   rds_inbound = [
     {
@@ -40,14 +102,11 @@ module "db" {
       port        = 22
       protocol    = "tcp"
       cidr_blocks = ["${module.networking.bastion_internal_ip}/32"]
-
-    }
-    ,
+    },
     {
       port        = 0
       protocol    = "all"
       cidr_blocks = ["192.168.0.0/16"]
-
     }
   ]
 
@@ -57,8 +116,17 @@ module "db" {
   }
 }
 
-provider "kubernetes" {
-  host                   = module.eks.endpoint
-  cluster_ca_certificate = module.eks.cacert
-  token                  = module.eks.token
+data "aws_vpc" "default" {
+  default = true
 }
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+provider "kubernetes" {
+  host                   = var.kubernetes_host
+  cluster_ca_certificate = var.kubernetes_cluster_ca_certificate
+  token                  = var.kubernetes_token
+}
+
--- AWS/envs/variables.tf
+++ AWS/envs/variables.tf
@@ -92,3 +92,74 @@ variable "outbound_all" {
     cidr_blocks = list(string)
   }))
 }
+
+
+variable "identifier" {
+  type    = string
+  default = "demo"
+}
+
+variable "engine" {
+  type    = string
+  default = "postgres"
+}
+
+variable "engine_version" {
+  type    = string
+  default = "14"
+}
+
+variable "instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+
+variable "allocated_storage" {
+  type    = string
+  default = "20"
+}
+
+variable "max_allocated_storage" {
+  type    = string
+  default = "100"
+}
+
+variable "username" {
+  type    = string
+  default = "postgres"
+}
+
+variable "password" {
+  type    = string
+  default = "postgres"
+}
+
+variable "port" {
+  type    = string
+  default = "5432"
+}
+
+variable "multi_az" {
+  type    = bool
+  default = false
+}
+
+variable "publicly_accessible" {
+  type    = bool
+  default = false
+}
+
+variable "deletion_protection" {
+  type    = bool
+  default = false
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+variable "security_group_tag" {
+  type    = string
+  default = "my-security-group"
+}
