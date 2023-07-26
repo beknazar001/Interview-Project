e1eaa13f907b796e3e4b1f0441619a6c9302e16d
Beknazar Saitov <beknazarsaitov@Beki.local>
2023-07-27 02:40:47 +0600

    change

--- AWS/envs/backend.tf
+++ AWS/envs/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "interview-project-dev-bucket-1234"
-    key    = "terraform/dev/"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "interview-project-dev-bucket-1234"
+#     key    = "terraform/dev/"
+#     region = "us-east-1"
+#   }
+# }
