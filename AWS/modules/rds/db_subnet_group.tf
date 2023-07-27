29c1d2f5eb74812bc0de4202f7cdd941bbe621a7
Nurbolot0101 <123730518+Nurbolot0101@users.noreply.github.com>
2023-07-28 00:15:48 +0600

    Update db_subnet_group.tf

--- AWS/modules/rds/db_subnet_group.tf
+++ AWS/modules/rds/db_subnet_group.tf
@@ -1,4 +1,4 @@
-resource "aws_db_subnet_group" "db_sg" {
+resource "aws_db_subnet_group" "db_sgg" {
   name        = var.subnet_group_name
   description = var.subnet_group_description
   subnet_ids  = var.subnet_ids
