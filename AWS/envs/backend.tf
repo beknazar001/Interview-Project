1cc066641069dfd186f22ad77fb5074acfdb99b8
Nurbolot0101 <123730518+Nurbolot0101@users.noreply.github.com>
2023-08-04 01:28:47 +0600

    Update backend.tf

--- AWS/envs/backend.tf
+++ AWS/envs/backend.tf
@@ -1,7 +1,7 @@
- terraform {
-   backend "s3" {
-     bucket = "interview-project-dev-bucket-1234"
-     key    = "Interview-Project/AWS/envs/backend.tf"
-     region = "us-east-1"
-   }
- }
+# terraform {
+# backend "s3" {
+# bucket = "interview-project-dev-bucket-1234"
+#  key    = "Interview-Project/AWS/envs/backend.tf"
+# region = "us-east-1"
+#}
+#}
